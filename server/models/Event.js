b2df73793578d1924646359db009a48d63a01a29
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-29 05:12:15 +0000

    event details

--- server/models/Event.js
+++ server/models/Event.js
@@ -8,3 +8,31 @@ const commentSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
+const eventSchema = new mongoose.Schema({
+  title: { type: string, required: true, trim: true },
+  description: { type: string, required: true },
+  category: {
+    type: string,
+    required: true,
+    enum: [
+      "Music",
+      "Sports",
+      "Tech",
+      "Education",
+      "Community",
+      "Food",
+      "Other",
+    ],
+  },
+  date: { type: Date, required: true },
+  time: { type: string, required: true },
+  location: { type: string, required: true, trim: true },
+  image: { type: string, default: "" },
+  capacity: {type:Number, defualt:0},
+  organizer:{ type: mongoose.Schema.Types.ObjectId,ref="User",required:true},
+  attendees: [{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
+  comments: [commentSchema],
+},
+{timestamps:true}
+);
+export default mongoose.model("Event",eventSchema);
