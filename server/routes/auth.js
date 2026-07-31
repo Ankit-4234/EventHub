9f1af5d29321eb6e1baf77435bfe2a1a3906eabc
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-31 13:18:07 +0000

    register

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,5 +14,22 @@ router.post("/register",async(req,res)=>{
         if(!name || !email || !password){
             return res.staus(400).json({message: "all fields are required"});
         }
+        const existing = await User.findone({email: email.toLowerCase()});
+        if(existing){
+            return res.status(400).json({message:"Email already registered"});
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = await User.create({name,email, password:hashedPassword});
+        res.status(201).json({
+            token: generateToken(user._id),
+            user: {id: user._id, name: user.name, email: user.email},
+        });
+    }catch(err){
+        res.status(500).json({message: err.message});
     }
 });
+router.post('/login', async(req,res)=>{
+    try{
+        
+    }
+})
