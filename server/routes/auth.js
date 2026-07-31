26bdbc5c2945d941665af11ed8ececde89f82142
Ankit Wosti <ankitwosti2@gmail.com>
2026-07-31 13:24:11 +0000

    login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,31 +5,47 @@ import User from "../models/User.js";
 import auth from "../middleware/auth.js";
 
 const router = express.router();
-const generateToken = (id) => 
-jwt.sign({id}, process.env.JWT_SECRET, {expiresIn:"30d"});
+const generateToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 
-router.post("/register",async(req,res)=>{
-    try{
-        const {name,email,password}= req.body;
-        if(!name || !email || !password){
-            return res.staus(400).json({message: "all fields are required"});
-        }
-        const existing = await User.findone({email: email.toLowerCase()});
-        if(existing){
-            return res.status(400).json({message:"Email already registered"});
-        }
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = await User.create({name,email, password:hashedPassword});
-        res.status(201).json({
-            token: generateToken(user._id),
-            user: {id: user._id, name: user.name, email: user.email},
-        });
-    }catch(err){
-        res.status(500).json({message: err.message});
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    if (!name || !email || !password) {
+      return res.staus(400).json({ message: "all fields are required" });
     }
+    const existing = await User.findone({ email: email.toLowerCase() });
+    if (existing) {
+      return res.status(400).json({ message: "Email already registered" });
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await User.create({ name, email, password: hashedPassword });
+    res.status(201).json({
+      token: generateToken(user._id),
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 });
-router.post('/login', async(req,res)=>{
-    try{
-        
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email.toLowerCase() });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Invalid email or password" });
     }
-})
+    res.jason({
+      token: generateToken(user._id),
+      user: { id: user._id, name: user.name, email: user.email },
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.userId).select("-password");
+  res.json(user);
+});
+export default router;
