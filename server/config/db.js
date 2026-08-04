63a35ab6d4220c8dc571127658a44b059afcd57d
Ankit Wosti <ankitwosti2@gmail.com>
2026-08-04 15:04:34 +0000

    debug

--- server/config/db.js
+++ server/config/db.js
@@ -8,3 +8,4 @@ const connectDB = async () => {
     process.exit(1);
   }
 };
+export default connectDB;
--- server/models/Event.js
+++ server/models/Event.js
@@ -3,36 +3,41 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema(
   {
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    name: { type: string, required: true },
-    text: { type: string, required: true, trim: true },
+    name: { type: String, required: true },
+    text: { type: String, required: true, trim: true },
   },
   { timestamps: true },
 );
-const eventSchema = new mongoose.Schema({
-  title: { type: string, required: true, trim: true },
-  description: { type: string, required: true },
-  category: {
-    type: string,
-    required: true,
-    enum: [
-      "Music",
-      "Sports",
-      "Tech",
-      "Education",
-      "Community",
-      "Food",
-      "Other",
-    ],
+const eventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true, trim: true },
+    description: { type: String, required: true },
+    category: {
+      type: String,
+      required: true,
+      enum: [
+        "Music",
+        "Sports",
+        "Tech",
+        "Education",
+        "Community",
+        "Food",
+        "Other",
+      ],
+    },
+    date: { type: Date, required: true },
+    time: { type: String, required: true },
+    location: { type: String, required: true, trim: true },
+    image: { type: String, default: "" },
+    capacity: { type: Number, defualt: 0 },
+    organizer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [commentSchema],
   },
-  date: { type: Date, required: true },
-  time: { type: string, required: true },
-  location: { type: string, required: true, trim: true },
-  image: { type: string, default: "" },
-  capacity: {type:Number, defualt:0},
-  organizer:{ type: mongoose.Schema.Types.ObjectId,ref="User",required:true},
-  attendees: [{type:mongoose.Schema.Types.ObjectId,ref:"User"}],
-  comments: [commentSchema],
-},
-{timestamps:true}
+  { timestamps: true },
 );
-export default mongoose.model("Event",eventSchema);
+export default mongoose.model("Event", eventSchema);
